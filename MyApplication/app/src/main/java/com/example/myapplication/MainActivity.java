206cd06010d54eef12dd9578e5d18e45029b061b
emanuel <emanuel@manu-pc.bbrouter>
2024-04-07 16:50:30 -0300

    simple projects

---
+++ MyApplication/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,29 @@
+package com.example.myapplication;
+
+import android.os.Bundle;
+
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.view.View;
+
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+    protected void onHelloClick(View view){
+
+        TextView textWellcomido  = findViewById(R.id.textWelcome);
+        textWellcomido.setText("seja wellcomido");
+
+    }
+
+
+}
